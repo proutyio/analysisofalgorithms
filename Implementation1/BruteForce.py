e36ee27789abf75f8a01361c7ae2481a0f4631bf
proutyio <kyle@prouty.io>
2017-01-22 23:53:18 -0800

    distance formula working

--- Implementation1/BruteForce.py
+++ Implementation1/BruteForce.py
@@ -1,3 +1,4 @@
+import math
 '''
 	CS 325 - Implementation 1
 	Kyle Prouty
@@ -9,6 +10,18 @@ def readFile():
 	lst =[]
 	with open("example.input") as file:
 		lst = [tuple(map(int, l.split(' '))) for l in file]
-	print lst
+	return lst
 
-readFile()
+
+def distance(tupA, tupB):
+	return math.sqrt((tupB[0]-tupA[0])**2 + (tupB[1]-tupA[1])**2)
+
+
+def printDistances():
+	lst = readFile()
+	for tupA in lst:
+		for tupB in lst:
+			print distance(tupA, tupB)
+
+
+printDistances()
