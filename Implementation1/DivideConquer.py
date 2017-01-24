2a84998c94e21c9b244507250ac1ffcf8e090a20
proutyio <kyle@prouty.io>
2017-01-24 10:26:57 -0800

    progress

--- Implementation1/DivideConquer.py
+++ Implementation1/DivideConquer.py
@@ -16,30 +16,34 @@ def readFile():
 		points = [tuple(map(int, l.split(' '))) for l in file]
 	return points
 
+
 def distance(pointA,pointB):
 	return math.sqrt((pointB[0]-pointA[0])**2 + (pointB[1]-pointA[1])**2)
 
-def compare(pointA,pointB):
-	d = distance(pointA,pointB)
-	global min
-	if d < min:
-		min = d
-		#print min
+
+def min(a,b):
+	if(a < b):
+		return a
+	else:
+		return b
+
+
+def closestCrossPairs():
+	print
+
 
 def divideAndConquer(points):	
-	if len(points) == 2: 
-		compare(points[0], points[1])
+	if len(points) <= 3: 
+		return distance(points[0], points[1])
 	else:
 		mid = len(points)/2
 		left = points[:mid]
 		right = points[-mid:]
-		print left, right
 
-		compare(left[0],right[0])
-		global min
-		print min
-		minleft = divideAndConquer(left)
-		minRight = divideAndConquer(right)
+		d1 = divideAndConquer(left)
+		d2 = divideAndConquer(right)
+		d = min(d1,d2)
+		return d
+
 		
-divideAndConquer(readFile())
-print min
+print divideAndConquer(readFile())
