1a5910ec202fcdc8f93f408f4ecc043d8e4806da
proutyio <kyle@prouty.io>
2017-01-24 22:57:27 -0800

    progress, i think closestCrossPairs is working right

--- Implementation1/DivideConquer.py
+++ Implementation1/DivideConquer.py
@@ -9,6 +9,7 @@ import math
 
 inputfile = "example.input"
 
+delta =0
 
 def readFile():
 	pts=[]
@@ -27,44 +28,41 @@ def min(a,b):
 	else:
 		return b
 
+#def minPts(p1,p2)
+	#a = distance(p1)
 
-def closestCrossPairs(pts,delta):
-	dm = delta
+
+def closestCrossPairs(pts,dt):
+	dm = dt
 	for i in range(1, len(pts)-1):
 		j = i+1
-		while pts[i+1][1] - pts[i][1] <= delta and j <= len(pts):
+		while pts[i+1][1] - pts[i][1] <= dt and j <= len(pts):
 			d = distance(pts[i], pts[i+1])
-			print d
 			dm = min(d, dm)
-			print dm
 			j = j+1
 	return dm
 
 
 
 def divideAndConquer(pts):	
-	if len(pts) <= 2:
-		return (pts[0], pts[1])  
-		#return distance(pts[0], pts[1])
+	if len(pts) < 3:
+		return pts  
 	else:
-		pts.sort(key=lambda x: x[0]) #sort points by x value - (nlogn)
+		pts.sort(key=lambda s: s[0]) #sort points by x value - (nlogn)
 		m = len(pts)/2				
 		l = pts[:m]
 		r = pts[-m:]
-		print pts
 
 		d1 = divideAndConquer(l)
 		d2 = divideAndConquer(r)
-		print d1
-		print d2
 
-		#d = min(d1,d2)
-		#print d
-		#return d
+		if d1 and d2 is not None:
+			global delta
+			#delta = min( distance(d1[0],d1[1]), distance(d2[0],d2[1]) )
+
+		pts.sort(key=lambda s: s[1])
+		dm = closestCrossPairs(pts,1)
+		return dm
 
 		
-#print divideAndConquer(readFile())
-pts = readFile()
-pts.sort(key=lambda x: x[1])
-#print pts
-print closestCrossPairs(pts, 6)
+print divideAndConquer(readFile())
