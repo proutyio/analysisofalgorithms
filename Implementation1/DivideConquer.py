2112c145ef33b375f2c6e5d4012e1f1732793bfe
proutyio <kyle@prouty.io>
2017-01-24 22:14:23 -0800

    working on closestCrossPairs

--- Implementation1/DivideConquer.py
+++ Implementation1/DivideConquer.py
@@ -21,32 +21,50 @@ def distance(p1,p2):
 	return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 
 
-def min(p1,p2):
-	a = distance(p1[0],p1[1])
-	b = distance(p2[0],p2[1])
+def min(a,b):
 	if(a < b):
 		return a
 	else:
 		return b
 
 
-def closestCrossPairs():
-	print
+def closestCrossPairs(pts,delta):
+	dm = delta
+	for i in range(1, len(pts)-1):
+		j = i+1
+		while pts[i+1][1] - pts[i][1] <= delta and j <= len(pts):
+			d = distance(pts[i], pts[i+1])
+			print d
+			dm = min(d, dm)
+			print dm
+			j = j+1
+	return dm
+
 
 
 def divideAndConquer(pts):	
-	if len(pts) <= 3: 
-		return distance(pts[0], pts[1])
+	if len(pts) <= 2:
+		return (pts[0], pts[1])  
+		#return distance(pts[0], pts[1])
 	else:
 		pts.sort(key=lambda x: x[0]) #sort points by x value - (nlogn)
 		m = len(pts)/2				
 		l = pts[:m]
 		r = pts[-m:]
+		print pts
 
 		d1 = divideAndConquer(l)
 		d2 = divideAndConquer(r)
+		print d1
+		print d2
+
 		#d = min(d1,d2)
+		#print d
 		#return d
 
 		
-print divideAndConquer(readFile())
+#print divideAndConquer(readFile())
+pts = readFile()
+pts.sort(key=lambda x: x[1])
+#print pts
+print closestCrossPairs(pts, 6)
