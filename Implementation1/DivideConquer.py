dbf74f06e4c0147cb7138c0aa5e40b577284878b
proutyio <kyle@prouty.io>
2017-01-24 23:30:37 -0800

    seems to be working and outputing correct min pts, need to fix delta calculation

--- Implementation1/DivideConquer.py
+++ Implementation1/DivideConquer.py
@@ -9,7 +9,8 @@ import math
 
 inputfile = "example.input"
 
-delta =0
+delta = 0
+min_pts = []
 
 def readFile():
 	pts=[]
@@ -22,11 +23,12 @@ def distance(p1,p2):
 	return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 
 
-def min(a,b):
-	if(a < b):
-		return a
+def min(d,dm, pts):
+	if(d <= dm):
+		min_pts.append(pts)
+		return d
 	else:
-		return b
+		return dm
 
 
 def closestCrossPairs(pts,dt):
@@ -35,8 +37,11 @@ def closestCrossPairs(pts,dt):
 		j = i+1
 		while pts[i+1][1] - pts[i][1] <= dt and j <= len(pts):
 			d = distance(pts[i], pts[i+1])
-			dm = min(d, dm)
+			cur_pts = (pts[i], pts[i+1])
+			#print cur_pts
+			dm = min(d, dm, cur_pts)
 			j = j+1
+			break
 	return dm
 
 
@@ -53,8 +58,7 @@ def divideAndConquer(pts):
 		d1 = divideAndConquer(l)
 		d2 = divideAndConquer(r)
 
-		if d1 and d2 is not None:
-			global delta
+		#if d1 and d2 is not None:
 			#delta = min( distance(d1[0],d1[1]), distance(d2[0],d2[1]) )
 
 		pts.sort(key=lambda s: s[1])
@@ -63,3 +67,6 @@ def divideAndConquer(pts):
 
 		
 print divideAndConquer(readFile())
+min_pts.sort(key=lambda s:s[0])
+for pt in min_pts:
+	print pt[0], pt[1]
