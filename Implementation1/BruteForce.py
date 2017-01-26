1e056dd86b0b7ce97cf5c3a23be2f35b5647e8dd
proutyio <kyle@prouty.io>
2017-01-26 01:46:16 -0800

    fixed and cleaned up BruteForce

--- Implementation1/BruteForce.py
+++ Implementation1/BruteForce.py
@@ -1,4 +1,4 @@
-import math
+import math, sys
 '''
 	CS 325 - Implementation 1
 		Brute Force
@@ -6,42 +6,49 @@ import math
 	Kyle Prouty
 	Winter 2017
 '''
-inputfile = "points.input"
+minlst=[]
+
+if(len(sys.argv)>1):
+	inputfile = sys.argv[1]
+else:
+	inputfile = 'example.input'
+
 
 def readFile():
-	lst =[]
 	with open(inputfile) as file:
-		lst = [tuple(map(int, l.split(' '))) for l in file]
-	return lst
+		pts = [tuple(map(int, l.split(' '))) for l in file]
+	pts.sort(key=lambda s:s[1])
+	pts.sort(key=lambda s:s[0])
+	return pts
 
 
-def distance(tupA, tupB):
-	return math.sqrt((tupB[0]-tupA[0])**2 + (tupB[1]-tupA[1])**2)
+def distance(p1, p2):
+	return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 
 
-def bruteForce(lst):
+def bruteForce(pts):
+	global minlst
 	min = float("inf")
-	for tupA in lst:
-		for tupB in lst:
-			d = distance(tupA, tupB)
-			if (d != 0) and (d <= min): 
+	for p1 in pts:
+		for p2 in pts:
+			d = distance(p1, p2)
+			if(d < min) and (d!=0): 
 				min = d
+				minlst = [[p1,p2]]
+			elif(d == min):
+				if not isDuplicate(p1,p2):
+					minlst.append([p1,p2])
 	return min
+
+
+def isDuplicate(p1, p2):
+	global minlst
+	for p in minlst:
+		if((p[0]==p1) and (p[1]==p2)) or ((p[0]==p2) and (p[1]==p1)):
+			return True
+	return False
 	
 
-def printMinDistance(lst):
-	min = bruteForce(lst)
-	print min
-	
-	'''
-	lst.sort(key=lambda x: x[1])
-	lst.sort(key=lambda x: x[0])
-	for a in lst:
-		for b in lst:
-			if distance(a,b) == min:
-				tmp = [a,b]
-				if tmp[0][1] > tmp[1][1]:
-					print b, a
-	'''
-
-printMinDistance( readFile() )
+print bruteForce( readFile() )
+for (a,b) in minlst:
+	print a,b
--- Implementation1/GeneratePoints.py
+++ Implementation1/GeneratePoints.py
@@ -7,8 +7,8 @@ def generatePoints():
 
 	f = open('points.input', 'w')
 	for i in range(0,total):
-		x = randint(0,100)
-		y = randint(101,200)
+		x = randint(0,1000)
+		y = randint(2000,3000)
 		f.write(str(x)+" "+str(y)+"\n")
 	f.close()
 	print
