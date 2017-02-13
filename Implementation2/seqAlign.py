e8dd87d8d01ef41d3322024c65c79d8c8713743a
Levi Willmeth <levi.willmeth@gmail.com>
2017-02-12 22:41:57 -0800

    Begin prototyping an implementation 2

---
+++ Implementation2/seqAlign.py
@@ -0,0 +1,40 @@
+import sys
+
+if(len(sys.argv)>2):
+    topWord = '-'+sys.argv[1]
+    sideWord = '-'+sys.argv[2]
+else:
+    topWord = '-ATCC'
+    sideWord = '-TCAC'
+
+def printA():
+    ''' Display the array as it would appear on paper '''
+    print '#',
+    for x in topWord:
+        print x,
+    print ''
+    for y in range(lenSide):
+        for x in range(lenTop):
+            if x is 0:
+                print sideWord[y],
+            print A[x][y],
+        print ''
+
+lenTop = len(topWord)
+lenSide = len(sideWord)
+
+A = [[x+y for x in range(lenTop)] for y in range(lenSide)]
+A[0][0] = 0
+
+for x in range(1,lenTop):
+    # print 'x = ', x
+    for y in range(1,lenSide):
+        A[x][y] = min(
+            A[x-1][y] + 1,
+            A[x][y-1] + 1,
+            A[x-1][y-1] + (2, 0)[topWord[x] == sideWord[y]]
+        )
+        # print [x,y],(A[x][y], A[x-1][y] + 1, A[x][y-1] + 1, A[x-1][y-1] + (2, 0)[topWord[x] == sideWord[y]]), topWord[x], sideWord[y], A[x]
+
+print 'The resulting array:'
+printA()
