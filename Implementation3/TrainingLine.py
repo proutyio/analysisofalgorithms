9703c6b766a6ceb6f77933788b93950961cdee9f
Megadrew <morrilan@oregonstate.edu>
2017-03-10 09:39:32 -0800

    Beginning Implimentation 3

---
+++ Implementation3/TrainingLine.py
@@ -0,0 +1,8 @@
+# Practice Implementation
+# (1; 3); (2; 5); (3; 7); (5; 11); (7; 14); (8; 15); (10; 19)
+x = {1, 2, 3, 5, 7, 8, 10}
+y = {3, 5, 7, 11, 14, 15, 19}
+X = float(sum (x))/len(x) # Average X-val
+Y = float(sum (y))/len(y) # Average Y-val
+print X
+print Y
